1f6b9b7516c6216ec1a00f336ae21fc54505fbd6
root <root@Ubuntu-jammy-latest-amd64-base.zst>
2026-08-05 01:26:02 +0000

    disable payment network calls in demo environment

--- client-portal/src/pages/private/platform/platformMenus/payments/connect-banks/helpers.ts
+++ client-portal/src/pages/private/platform/platformMenus/payments/connect-banks/helpers.ts
@@ -1,11 +1,7 @@
 import { ReigionData } from "./types";
 
 export async function reigionDetector(): Promise<ReigionData | undefined> {
-  try {
-    const response = await fetch("http://ip-api.com/json");
-    const result: Promise<ReigionData> = await response.json();
-    return result;
-  } catch (error) {
-    console.error(error);
-  }
+  // Funding UI is disabled for Demo accounts; do not call third-party IP or
+  // payment services just to infer a banking region.
+  return undefined;
 }
