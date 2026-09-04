fc16b803839b4e75a5f31e8c5ce05724b4876761
Ralph Appolon <appolon1908@gmail.com>
2026-09-04 18:05:27 -0400

    feat(email): expose preferences in notification settings

--- client-portal/src/pages/private/platform/platformMenus/notifications/NotificationsMenu.tsx
+++ client-portal/src/pages/private/platform/platformMenus/notifications/NotificationsMenu.tsx
@@ -1,11 +1,12 @@
-
-import Toggle from "../../../../../components/toggle/Toggle";
-import "./notificationsMenu.scss";
 import { INotification } from "@interfaces";
-import { updateNotificationList } from "@store/slices/notification";
 import { useAppDispatch, useAppSelector } from "@store/hooks";
-import { useCookies } from "react-cookie";
+import { updateNotificationList } from "@store/slices/notification";
 import useNotificationToggle from "api/notification/useToggleNotification";
+import Toggle from "../../../../../components/toggle/Toggle";
+import { useCookies } from "react-cookie";
+
+import EmailPreferencesPanel from "./EmailPreferencesPanel";
+import "./notificationsMenu.scss";
 
 interface NotificationsMenuProps {}
 
@@ -19,32 +20,46 @@ const NotificationsMenu: React.FunctionComponent<
     onSuccess: (data) => {
       dispatch(updateNotificationList(data));
     },
-    onError: (error) => { },
+    onError: (_error) => {},
   });
-  const handleNotificationToggle = (data: INotification, isEnabled: boolean) => {
 
+  const handleNotificationToggle = (
+    data: INotification,
+    isEnabled: boolean,
+  ) => {
     mutate({
-      data: {notification_id: data?.id, is_enabled: isEnabled},
+      data: { notification_id: data?.id, is_enabled: isEnabled },
       token: cookies.access_token,
     });
   };
+
   return (
     <div className="notificationsMenu">
       <div className="notificationsMenuSection">
         <p className="notificationsSectionTitle">
           Select the notifications you want to receive
         </p>
-        {notificationList.map((notificationData: INotification, _i: number) => (
+        {notificationList.map((notificationData: INotification) => (
           <Toggle
-            key={_i}
+            key={notificationData.id}
             label={notificationData?.name}
-            onChange={(checked) => handleNotificationToggle(notificationData, checked)}
+            onChange={(checked) =>
+              handleNotificationToggle(notificationData, checked)
+            }
             subtext={notificationData?.description}
             defaultChecked={notificationData?.is_enabled}
-            infoText={notificationData?.name === "Push Notifications" ? "Why should I receive them?" :""}
+            disabled={isPending}
+            infoText={
+              notificationData?.name === "Push Notifications"
+                ? "Why should I receive them?"
+                : ""
+            }
           />
         ))}
-       
+      </div>
+
+      <div className="notificationsMenuSection">
+        <EmailPreferencesPanel />
       </div>
     </div>
   );
